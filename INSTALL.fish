94b6bc61d5cff3cd2416552c0832b2d043220118
Patrick Meredith <pmeredit@gmail.com>
2018-04-09 12:30:02 -0400

    omf stuff

---
+++ INSTALL.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+if not type -q stow
+     set un (uname -o ^ /dev/null; or uname -s)
+	if test $un = "Cygwin"
+	    cp apt-cyg /usr/bin
+	    apt-cyg install stow
+        else if test $un = "Darwin"
+	    brew install stow
+	else #assuming Ubuntu for now, come back later
+  	    sudo apt-get install stow
+     end
+end
+
+for i in (ls | grep -v INSTALL.sh | grep -v apt-cyg | grep -v INSTALL.fish | grep -v omf.fish | grep -v omf.fish | grep -v README.md)
+    echo "stowing $i ..."
+    stow $i
+end
+
+./omf.fish --path=~/.local/share/omf --config=~/.config/omf
