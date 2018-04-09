035818ef90182612c0b742f1246028f61942e4d2
Patrick Meredith <pmeredit@gmail.com>
2018-04-09 11:32:40 -0400

    improve spacing

--- config/.config/fish/functions/git_compare_remotes.fish
+++ config/.config/fish/functions/git_compare_remotes.fish
@@ -6,9 +6,9 @@ function git_compare_remotes
         set res (echo (git rev-list --left-right --count (git_current_branch)...$remote/(git_current_branch) ^ /dev/null) | string split '')
         if test (count $res) -gt 2
 	    if test $remote = $upstream
-	        set out "$out *$remote: "
+	        set out "$out *$remote:"
 	    else
-	        set out "$out $remote: "
+	        set out "$out $remote:"
             end
 	    set old $out
 	    if test $res[1] != "0"
